e1d8d0d39bec17019e74f04663c84fc88a72a2ec
Your Name <you@example.com>
2022-03-15 21:21:06 -0500

    controlar errores servidor consola

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,10 +6,37 @@ Console.WriteLine("Proyecto iniciado");
 Console.WriteLine("Es este el servidor? ingrese 1 para afirmar");
 string respuesta = Console.ReadLine();
 if (respuesta == "1")
-{
-    Console.WriteLine("Ingrese la Ip: ");
-    string ip = Console.ReadLine();
-    Console.WriteLine("Ingrese el puerto: ");
-    int port = Int32.Parse(Console.ReadLine());
-    var servidor = new ServerProgram(ip, port);
+{while (true)
+    {
+        try
+        {
+            Console.WriteLine("Ingrese la Ip: ");
+            string ip = Console.ReadLine();
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Ingrese el puerto: ");
+                    int port = Int32.Parse(Console.ReadLine());
+                    break;
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine("Ingreso erroneo, el valor para el puerto deben ser solo numeros.");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+                
+            }
+            var servidor = new ServerProgram(ip, port);
+            break;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error al tratar de crear el servidor, verifique que su ip sea correcto o el puerto este disponible.");
+            Console.ReadLine();
+            Console.Clear();
+        }
+    }
+   
 }
