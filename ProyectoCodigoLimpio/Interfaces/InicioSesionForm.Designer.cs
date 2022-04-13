6963f2ffff6233758c48089deb2c5a595ca2e54c
Fasbox <fabianchavarria03@gmail.com>
2022-04-13 00:17:32 -0500

    Creación interfaces de inicio y registro

---
+++ ProyectoCodigoLimpio/Interfaces/InicioSesionForm.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace ProyectoCodigoLimpioClient.Interfaces
+{
+    partial class InicioSesionForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonVolverAlInicio = new System.Windows.Forms.Button();
+            this.buttonRegistrarse = new System.Windows.Forms.Button();
+            this.checkBoxMostrarContraseña = new System.Windows.Forms.CheckBox();
+            this.textBoxContraseña = new System.Windows.Forms.TextBox();
+            this.labelYaTienesCuenta = new System.Windows.Forms.Label();
+            this.labelContraseña = new System.Windows.Forms.Label();
+            this.textBoxUsername = new System.Windows.Forms.TextBox();
+            this.labelUsername = new System.Windows.Forms.Label();
+            this.pictureBoxLogo = new System.Windows.Forms.PictureBox();
+            this.labelComencemos = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonVolverAlInicio
+            // 
+            this.buttonVolverAlInicio.BackColor = System.Drawing.Color.White;
+            this.buttonVolverAlInicio.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonVolverAlInicio.Font = new System.Drawing.Font("Nirmala UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.buttonVolverAlInicio.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(30)))), ((int)(((byte)(59)))));
+            this.buttonVolverAlInicio.Location = new System.Drawing.Point(30, 446);
+            this.buttonVolverAlInicio.Name = "buttonVolverAlInicio";
+            this.buttonVolverAlInicio.Size = new System.Drawing.Size(224, 36);
+            this.buttonVolverAlInicio.TabIndex = 16;
+            this.buttonVolverAlInicio.Text = "REGISTRARSE";
+            this.buttonVolverAlInicio.UseVisualStyleBackColor = false;
+            // 
+            // buttonRegistrarse
+            // 
+            this.buttonRegistrarse.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(30)))), ((int)(((byte)(59)))));
+            this.buttonRegistrarse.FlatAppearance.BorderSize = 0;
+            this.buttonRegistrarse.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonRegistrarse.Font = new System.Drawing.Font("Nirmala UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.buttonRegistrarse.ForeColor = System.Drawing.Color.White;
+            this.buttonRegistrarse.Location = new System.Drawing.Point(30, 375);
+            this.buttonRegistrarse.Name = "buttonRegistrarse";
+            this.buttonRegistrarse.Size = new System.Drawing.Size(224, 36);
+            this.buttonRegistrarse.TabIndex = 17;
+            this.buttonRegistrarse.Text = "INICIAR SESIÓN";
+            this.buttonRegistrarse.UseVisualStyleBackColor = false;
+            // 
+            // checkBoxMostrarContraseña
+            // 
+            this.checkBoxMostrarContraseña.AutoSize = true;
+            this.checkBoxMostrarContraseña.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.checkBoxMostrarContraseña.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.checkBoxMostrarContraseña.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.checkBoxMostrarContraseña.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(164)))), ((int)(((byte)(165)))), ((int)(((byte)(169)))));
+            this.checkBoxMostrarContraseña.Location = new System.Drawing.Point(109, 335);
+            this.checkBoxMostrarContraseña.Name = "checkBoxMostrarContraseña";
+            this.checkBoxMostrarContraseña.Size = new System.Drawing.Size(145, 21);
+            this.checkBoxMostrarContraseña.TabIndex = 15;
+            this.checkBoxMostrarContraseña.Text = "Mostrar Contraseña";
+            this.checkBoxMostrarContraseña.UseVisualStyleBackColor = true;
+            // 
+            // textBoxContraseña
+            // 
+            this.textBoxContraseña.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(211)))), ((int)(((byte)(212)))));
+            this.textBoxContraseña.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxContraseña.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBoxContraseña.Location = new System.Drawing.Point(30, 290);
+            this.textBoxContraseña.Multiline = true;
+            this.textBoxContraseña.Name = "textBoxContraseña";
+            this.textBoxContraseña.Size = new System.Drawing.Size(224, 28);
+            this.textBoxContraseña.TabIndex = 13;
+            // 
+            // labelYaTienesCuenta
+            // 
+            this.labelYaTienesCuenta.AutoSize = true;
+            this.labelYaTienesCuenta.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelYaTienesCuenta.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(164)))), ((int)(((byte)(165)))), ((int)(((byte)(169)))));
+            this.labelYaTienesCuenta.Location = new System.Drawing.Point(67, 426);
+            this.labelYaTienesCuenta.Name = "labelYaTienesCuenta";
+            this.labelYaTienesCuenta.Size = new System.Drawing.Size(151, 17);
+            this.labelYaTienesCuenta.TabIndex = 9;
+            this.labelYaTienesCuenta.Text = "¿No tienes una cuenta?";
+            // 
+            // labelContraseña
+            // 
+            this.labelContraseña.AutoSize = true;
+            this.labelContraseña.Font = new System.Drawing.Font("Nirmala UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelContraseña.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(164)))), ((int)(((byte)(165)))), ((int)(((byte)(169)))));
+            this.labelContraseña.Location = new System.Drawing.Point(30, 266);
+            this.labelContraseña.Name = "labelContraseña";
+            this.labelContraseña.Size = new System.Drawing.Size(96, 21);
+            this.labelContraseña.TabIndex = 10;
+            this.labelContraseña.Text = "Contraseña";
+            // 
+            // textBoxUsername
+            // 
+            this.textBoxUsername.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(211)))), ((int)(((byte)(212)))));
+            this.textBoxUsername.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxUsername.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBoxUsername.Location = new System.Drawing.Point(30, 226);
+            this.textBoxUsername.Multiline = true;
+            this.textBoxUsername.Name = "textBoxUsername";
+            this.textBoxUsername.Size = new System.Drawing.Size(224, 28);
+            this.textBoxUsername.TabIndex = 14;
+            // 
+            // labelUsername
+            // 
+            this.labelUsername.AutoSize = true;
+            this.labelUsername.Font = new System.Drawing.Font("Nirmala UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelUsername.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(164)))), ((int)(((byte)(165)))), ((int)(((byte)(169)))));
+            this.labelUsername.Location = new System.Drawing.Point(30, 202);
+            this.labelUsername.Name = "labelUsername";
+            this.labelUsername.Size = new System.Drawing.Size(87, 21);
+            this.labelUsername.TabIndex = 11;
+            this.labelUsername.Text = "Username";
+            // 
+            // pictureBoxLogo
+            // 
+            this.pictureBoxLogo.Image = global::ProyectoCodigoLimpioClient.Properties.Resources.logo_size;
+            this.pictureBoxLogo.Location = new System.Drawing.Point(71, 17);
+            this.pictureBoxLogo.Name = "pictureBoxLogo";
+            this.pictureBoxLogo.Size = new System.Drawing.Size(150, 115);
+            this.pictureBoxLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBoxLogo.TabIndex = 7;
+            this.pictureBoxLogo.TabStop = false;
+            // 
+            // labelComencemos
+            // 
+            this.labelComencemos.AutoSize = true;
+            this.labelComencemos.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelComencemos.Location = new System.Drawing.Point(30, 157);
+            this.labelComencemos.Name = "labelComencemos";
+            this.labelComencemos.Size = new System.Drawing.Size(100, 27);
+            this.labelComencemos.TabIndex = 6;
+            this.labelComencemos.Text = "Ingresa";
+            // 
+            // InicioSesionForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(285, 544);
+            this.Controls.Add(this.buttonVolverAlInicio);
+            this.Controls.Add(this.buttonRegistrarse);
+            this.Controls.Add(this.checkBoxMostrarContraseña);
+            this.Controls.Add(this.textBoxContraseña);
+            this.Controls.Add(this.labelYaTienesCuenta);
+            this.Controls.Add(this.labelContraseña);
+            this.Controls.Add(this.textBoxUsername);
+            this.Controls.Add(this.labelUsername);
+            this.Controls.Add(this.pictureBoxLogo);
+            this.Controls.Add(this.labelComencemos);
+            this.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(30)))), ((int)(((byte)(59)))));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "InicioSesionForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "InicioSesionForm";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button buttonVolverAlInicio;
+        private Button buttonRegistrarse;
+        private CheckBox checkBoxMostrarContraseña;
+        private TextBox textBoxContraseña;
+        private Label labelYaTienesCuenta;
+        private Label labelContraseña;
+        private TextBox textBoxUsername;
+        private Label labelUsername;
+        private PictureBox pictureBoxLogo;
+        private Label labelComencemos;
+    }
+}
