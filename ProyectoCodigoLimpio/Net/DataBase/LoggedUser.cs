8bb2df3b7df31b09d238f89eacf1e8b9066eb010
Your Name <you@example.com>
2022-04-12 21:53:54 -0500

    Clase conexion con la base de datos

---
+++ ProyectoCodigoLimpio/Net/DataBase/LoggedUser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ProyectoCodigoLimpioClient.Net.DataBase
+{
+    public class LoggedUser
+    {   
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        [BsonElement("nickname")]
+
+        public string Nickname { get; set; }
+
+        [BsonElement("password")]
+        public string Password { get; set; }
+
+        [BsonElement("messages")]
+        public List<string> Messages { get; set; }
+
+    }
+}
